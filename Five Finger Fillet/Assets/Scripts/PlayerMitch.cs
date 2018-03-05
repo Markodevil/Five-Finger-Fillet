f570b25a111f3ce47b7923d51dd8c575a8650245
Mitchell Cattini-Schultz <karate.1996@hotmail.com>
2018-03-05 13:53:08 +1100

    Knifes fall then rise (instantly) again.

--- Five Finger Fillet/Assets/Scripts/PlayerMitch.cs
+++ Five Finger Fillet/Assets/Scripts/PlayerMitch.cs
@@ -6,18 +6,19 @@ public class PlayerMitch : MonoBehaviour
 {
     public GameObject[] goKnife;
     public Rigidbody[] rbKnife;
-    private GameObject[] intialPos;
-    private Transform[] knifeInitPos;
+    private Vector3[] knifeInitPos;
     private Knife[] scpKnife;
+
+    // Public vars for the designers
+    public float fFallSpeed = 0;
 
     // Use this for initialization
     void Start()
     {
-        intialPos = new GameObject[5];
-        knifeInitPos = new Transform[5];
+        knifeInitPos = new Vector3[5];
 
         for (int i = 0; i < rbKnife.Length; ++i)
-            knifeInitPos[i] = intialPos[i].gameObject.transform.GetComponent<Transform>();
+            knifeInitPos[i] = rbKnife[i].transform.position;
 
         scpKnife = new Knife[5];
 
@@ -25,7 +26,6 @@ public class PlayerMitch : MonoBehaviour
         for (int i = 0; i < rbKnife.Length; ++i)
         {
             rbKnife[i] = gameObject.transform.GetChild(i).GetComponent<Rigidbody>();
-            knifeInitPos[i].transform.position = rbKnife[i].transform.position;
         }
     }
 
@@ -34,25 +34,20 @@ public class PlayerMitch : MonoBehaviour
     {
         knifeStabs();
         knifeResolve();
-        if (knifeInitPos[0].transform.position.y != 1.38f)
-        {
-
-        }
-
     }
 
     void knifeStabs()
     {
         if (Input.GetKeyDown(KeyCode.Q))
-            rbKnife[0].AddForce(-Vector3.up * 10, ForceMode.Impulse);
+            rbKnife[0].AddForce(-Vector3.up * fFallSpeed, ForceMode.Impulse);
         if (Input.GetKeyDown(KeyCode.W))
-            rbKnife[1].AddForce(-Vector3.up * 10, ForceMode.Impulse);
+            rbKnife[1].AddForce(-Vector3.up * fFallSpeed, ForceMode.Impulse);
         if (Input.GetKeyDown(KeyCode.E))
-            rbKnife[2].AddForce(-Vector3.up * 10, ForceMode.Impulse);
+            rbKnife[2].AddForce(-Vector3.up * fFallSpeed, ForceMode.Impulse);
         if (Input.GetKeyDown(KeyCode.R))
-            rbKnife[3].AddForce(-Vector3.up * 10, ForceMode.Impulse);
+            rbKnife[3].AddForce(-Vector3.up * fFallSpeed, ForceMode.Impulse);
         if (Input.GetKeyDown(KeyCode.T))
-            rbKnife[4].AddForce(-Vector3.up * 10, ForceMode.Impulse);
+            rbKnife[4].AddForce(-Vector3.up * fFallSpeed, ForceMode.Impulse);
 
     }
 
@@ -67,7 +62,7 @@ public class PlayerMitch : MonoBehaviour
         {
             if (scpKnife[i].bKnifeHitGround)
             {
-                rbKnife[i].transform.position = knifeInitPos[i].transform.position;
+                rbKnife[i].transform.position = knifeInitPos[i];
                 scpKnife[i].bKnifeHitGround = false;
             }
         }
