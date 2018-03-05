4dfce68ced15c4de18832519cc682991a5d9609f
Mitchell Cattini-Schultz <karate.1996@hotmail.com>
2018-03-06 10:56:29 +1100

    Fingers drop when they are sliced.

---
+++ Five Finger Fillet/Assets/Scripts/Fingers.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fingers : MonoBehaviour
+{
+    [HideInInspector]
+    public bool bGotStabbed;
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Knife")
+        {
+            bGotStabbed = true;
+            rb.useGravity = true;
+            rb.constraints = RigidbodyConstraints.None;
+        }
+    }
+}
