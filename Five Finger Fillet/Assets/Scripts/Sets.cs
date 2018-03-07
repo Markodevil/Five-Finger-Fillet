f3eeab244faedb172123a831d910e19930e023b3
Fryzeec <rward98@outlook.com>
2018-03-07 11:42:49 +1100

    Working

--- Five Finger Fillet/Assets/Scripts/Sets.cs
+++ Five Finger Fillet/Assets/Scripts/Sets.cs
@@ -3,69 +3,77 @@ using System.Collections.Generic;
 using System;
 using System.Threading;
 using UnityEngine;
-
-public class Sets : MonoBehaviour
+namespace SETS
 {
-    private enum Fingers
+    public class Sets : MonoBehaviour
     {
-        Finger1,
-        Finger2,
-        Finger3,
-        Finger4,
-        Finger5
-    };
+        public enum Fingers
+        {
+            Finger1,
+            Finger2,
+            Finger3,
+            Finger4,
+            Finger5
+        };
 
-    List<List<Fingers>> RoundRequiredFingers = new List<List<Fingers>>();
-    List<List<Fingers>> FingerSets = new List<List<Fingers>>();
+        List<List<Fingers>> RoundRequiredFingers = new List<List<Fingers>>();
+        List<List<Fingers>> FingerSets = new List<List<Fingers>>();
+        public Fingers currentFinger;
 
-    public int RoundsTotoal = 5;
+        public bool Q = false;
+        public bool W = false;
+        public bool E = false;
+        public bool R = false;
+        public bool T = false;
 
-    private int currentRound = 0;
-    private int currentFingureIndex = 0;
+        public bool setImage = true;
 
-    private int playerOnePoints = 0;
-    private int playerTwoPoints = 0;
+        public int RoundsTotoal = 5;
+        private int currentRound = 0;
+        private int currentFingureIndex = 0;
 
-
-
-    private bool playerOneSucceds = false;
-    private bool playerTwoSucceds = false;
-
-    private bool playerOneHasFailedThisRound = false;
-    private bool playerTwoHasFailedThisRound = false;
-
-
-    public float WaitStateTime = 1.0f;
-    private float waitStateTimer = 0.0f;
-    //private float keyPressTimer = 0.0f;
-
-    private enum State
-    {
-        STATE_WAIT,
-        STATE_PLAY,
-        STATE_END,
-    }
-
-    private State state;
+        private int playerOnePoints = 0;
+        private int playerTwoPoints = 0;
 
 
+        private bool playerOneSucceds = false;
+        private bool playerTwoSucceds = false;
 
-    // Use this for initialization
-    void Start()
-    {
-        //Random Round Sets
-        // 
-        // Set 1: q,w,e,r,t     Set 2: q,e,w,r,t      Set 3: q,r,w,e,t
-        // Set 4: q,t,w,e,r     Set 5: w,q,e,r,t      Set 6: w,e,q,r,t
-        // Set 7: w,r,q,e,t     Set 8: e,q,w,r,t      Set 9: e,w,q,r,t
-        // Set 10: e,r,q,w,t    Set 11:r,q,w,e,t      Set 12: r,w,q,e,t
-        // Set 13: r,e,q,w,t    Set 14: r,t,q,w,e     Set 15: t,q,w,e,r
-        // Set 16: t,w,q,e,r    Set 17: t,e,q,w,r     Set 18: t,r,q,w,e
-        // Set 19: t,r,e,w,q    Set 20: t,e,r,w,q
+        private bool playerOneHasFailedThisRound = false;
+        private bool playerTwoHasFailedThisRound = false;
 
 
-        //Set 1:
-        FingerSets.Add(new List<Fingers>
+        public float WaitStateTime = 1.0f;
+        private float waitStateTimer = 0.0f;
+        //private float keyPressTimer = 0.0f;
+
+        private enum State
+        {
+            STATE_WAIT,
+            STATE_PLAY,
+            STATE_END,
+        }
+
+        private State state;
+
+
+
+        // Use this for initialization
+        void Start()
+        {
+            //Random Round Sets
+            // 
+            // Set 1: q,w,e,r,t     Set 2: q,e,w,r,t      Set 3: q,r,w,e,t
+            // Set 4: q,t,w,e,r     Set 5: w,q,e,r,t      Set 6: w,e,q,r,t
+            // Set 7: w,r,q,e,t     Set 8: e,q,w,r,t      Set 9: e,w,q,r,t
+            // Set 10: e,r,q,w,t    Set 11:r,q,w,e,t      Set 12: r,w,q,e,t
+            // Set 13: r,e,q,w,t    Set 14: r,t,q,w,e     Set 15: t,q,w,e,r
+            // Set 16: t,w,q,e,r    Set 17: t,e,q,w,r     Set 18: t,r,q,w,e
+            // Set 19: t,r,e,w,q    Set 20: t,e,r,w,q
+
+
+            //Set 1:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger1,
             Fingers.Finger2,
@@ -74,8 +82,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger5
         });
 
-        //Set2:
-        FingerSets.Add(new List<Fingers>
+            //Set2:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger1,
             Fingers.Finger3,
@@ -84,8 +92,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger5
         });
 
-        //Set3:
-        FingerSets.Add(new List<Fingers>
+            //Set3:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger1,
             Fingers.Finger4,
@@ -94,8 +102,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger5
         });
 
-        //Set4:
-        FingerSets.Add(new List<Fingers>
+            //Set4:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger1,
             Fingers.Finger5,
@@ -104,8 +112,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger4
         });
 
-        //Set5:
-        FingerSets.Add(new List<Fingers>
+            //Set5:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger2,
             Fingers.Finger1,
@@ -114,8 +122,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger5
         });
 
-        //Set6:
-        FingerSets.Add(new List<Fingers>
+            //Set6:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger2,
             Fingers.Finger3,
@@ -124,8 +132,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger5
         });
 
-        //Set7:
-        FingerSets.Add(new List<Fingers>
+            //Set7:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger2,
             Fingers.Finger4,
@@ -134,8 +142,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger5
         });
 
-        //Set8:
-        FingerSets.Add(new List<Fingers>
+            //Set8:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger3,
             Fingers.Finger1,
@@ -144,8 +152,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger5
         });
 
-        //Set9:
-        FingerSets.Add(new List<Fingers>
+            //Set9:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger3,
             Fingers.Finger2,
@@ -154,8 +162,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger5
         });
 
-        //Set10:
-        FingerSets.Add(new List<Fingers>
+            //Set10:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger3,
             Fingers.Finger4,
@@ -164,8 +172,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger5
         });
 
-        //Set11:
-        FingerSets.Add(new List<Fingers>
+            //Set11:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger4,
             Fingers.Finger1,
@@ -174,8 +182,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger5
         });
 
-        //Set12:
-        FingerSets.Add(new List<Fingers>
+            //Set12:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger4,
             Fingers.Finger2,
@@ -184,8 +192,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger5
         });
 
-        //Set13:
-        FingerSets.Add(new List<Fingers>
+            //Set13:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger4,
             Fingers.Finger3,
@@ -194,8 +202,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger5
         });
 
-        //Set14:
-        FingerSets.Add(new List<Fingers>
+            //Set14:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger4,
             Fingers.Finger5,
@@ -204,8 +212,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger3
         });
 
-        //Set15:
-        FingerSets.Add(new List<Fingers>
+            //Set15:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger5,
             Fingers.Finger1,
@@ -214,8 +222,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger4
         });
 
-        //Set16:
-        FingerSets.Add(new List<Fingers>
+            //Set16:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger5,
             Fingers.Finger2,
@@ -224,8 +232,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger4
         });
 
-        //Set17:
-        FingerSets.Add(new List<Fingers>
+            //Set17:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger5,
             Fingers.Finger3,
@@ -234,8 +242,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger4
         });
 
-        //Set18:
-        FingerSets.Add(new List<Fingers>
+            //Set18:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger5,
             Fingers.Finger4,
@@ -244,8 +252,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger3
         });
 
-        //Set19:
-        FingerSets.Add(new List<Fingers>
+            //Set19:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger5,
             Fingers.Finger4,
@@ -254,8 +262,8 @@ public class Sets : MonoBehaviour
             Fingers.Finger1
         });
 
-        //Set20:
-        FingerSets.Add(new List<Fingers>
+            //Set20:
+            FingerSets.Add(new List<Fingers>
         {
             Fingers.Finger5,
             Fingers.Finger3,
@@ -264,127 +272,195 @@ public class Sets : MonoBehaviour
             Fingers.Finger1
         });
 
-        for (int i = 0; i < RoundsTotoal; ++i)
-        {
-            List<Fingers> set = FingerSets[UnityEngine.Random.Range(0, FingerSets.Count - 1)];
-            RoundRequiredFingers.Add(set);
-        }
-    }
-
-  
-    // Update is called once per frame
-    void Update()
-    {
-        switch(state)
-        {
-            case State.STATE_WAIT:
-                update_WaitState();
-                break;
-            case State.STATE_PLAY:
-                update_PlayState();
-                break;
-        }
-    }
-
-    void update_WaitState()
-    {
-        Fingers currentFinger = RoundRequiredFingers[currentRound][currentFingureIndex];
-        print(currentFinger);
-
-        if(playerOneSucceds == false)
-        {
-            playerOneSucceds = GetKeyWasPressed(currentFinger, 1);
-        }
-        if (playerTwoSucceds == false)
-        {
-            playerTwoSucceds = GetKeyWasPressed(currentFinger, 2);
+            for (int i = 0; i < RoundsTotoal; ++i)
+            {
+                List<Fingers> set = FingerSets[UnityEngine.Random.Range(0, FingerSets.Count - 1)];
+                RoundRequiredFingers.Add(set);
+            }
         }
 
 
-        //Transition
-        waitStateTimer += Time.deltaTime;
-        if(waitStateTimer > WaitStateTime)
+        // Update is called once per frame
+        void Update()
         {
-            waitStateTimer = 0.0f;
-            state = State.STATE_PLAY;
-        }
-   
-    }
-
-    void update_PlayState()
-    {
-        if (!playerOneHasFailedThisRound && playerOneSucceds == true)
-        {
-            print(playerOnePoints++);
-        }
-        else
-        {
-            playerOneHasFailedThisRound = true;
+            switch (state)
+            {
+                case State.STATE_WAIT:
+                    update_WaitState();
+                    break;
+                case State.STATE_PLAY:
+                    update_PlayState();
+                    break;
+                case State.STATE_END:
+                    End_State();
+                    break;
+            }
         }
 
-        if (!playerTwoHasFailedThisRound && playerTwoSucceds == true)
+        void update_WaitState()
         {
-            print(playerTwoPoints++);
-        }
-        else
-        {
-            playerTwoHasFailedThisRound = true;
-        }
+            Fingers currentFinger = RoundRequiredFingers[currentRound][currentFingureIndex];
+            int i = (int)RoundRequiredFingers[currentRound][currentFingureIndex];
 
-        playerOneSucceds = false;
-        playerTwoSucceds = false;
+            //sets image to be shown
+            setImage = SetKeyImage(currentFinger, 1);
 
-        currentFingureIndex++;
-        if (currentFingureIndex >= RoundRequiredFingers[currentRound].Count)
-        {
-            playerOneHasFailedThisRound = false;
-            playerTwoHasFailedThisRound = false;
-            Debug.Log("Round Over");
+            //copy for P2
 
-            currentFingureIndex = 0;
-            currentRound++;
-        }
-
-        //Transition
-        if (currentRound >= RoundRequiredFingers.Count)
-        {
-            state = State.STATE_END;
-        }
-        else
-        {
-            state = State.STATE_WAIT;
-        }
-    }
+            if (playerOneSucceds == false)
+            {
+                playerOneSucceds = GetKeyWasPressed(currentFinger, 1);
+            }
+            if (playerTwoSucceds == false)
+            {
+                playerTwoSucceds = GetKeyWasPressed(currentFinger, 2);
+            }
 
 
-    private bool GetKeyWasPressed(Fingers currentFinger, int v)
-    {
-        if (currentFinger == Fingers.Finger1)
-        {
-            if (v == 1) return Input.GetKey(KeyCode.Q);
-            if (v == 2) return Input.GetKey(KeyCode.Y);
+            //Transition
+            waitStateTimer += Time.deltaTime;
+            if (waitStateTimer > WaitStateTime)
+            {
+                waitStateTimer = 0.0f;
+                state = State.STATE_PLAY;
+            }
+
         }
-        if (currentFinger == Fingers.Finger2)
+
+        void End_State()
         {
-            if (v == 1) return Input.GetKey(KeyCode.W);
-            if (v == 2) return Input.GetKey(KeyCode.U);
+            Debug.Log("GAME OVER");
         }
-        if (currentFinger == Fingers.Finger3)
+
+        void update_PlayState()
         {
-            if (v == 1) return Input.GetKey(KeyCode.E);
-            if (v == 2) return Input.GetKey(KeyCode.I);
+
+
+            if (!playerOneHasFailedThisRound && playerOneSucceds == true)
+            {
+                print(playerOnePoints++);
+            }
+            else
+            {
+                playerOneHasFailedThisRound = true;
+            }
+
+            if (!playerTwoHasFailedThisRound && playerTwoSucceds == true)
+            {
+                print(playerTwoPoints++);
+            }
+            else
+            {
+                playerTwoHasFailedThisRound = true;
+            }
+
+            playerOneSucceds = false;
+            playerTwoSucceds = false;
+
+            currentFingureIndex++;
+            if (currentFingureIndex >= RoundRequiredFingers[currentRound].Count)
+            {
+                playerOneHasFailedThisRound = false;
+                playerTwoHasFailedThisRound = false;
+                Debug.Log("Round Over");
+
+                currentFingureIndex = 0;
+                currentRound++;
+            }
+
+            //Transition
+            if (currentRound >= RoundRequiredFingers.Count)
+            {
+                state = State.STATE_END;
+            }
+            else
+            {
+                state = State.STATE_WAIT;
+            }
         }
-        if (currentFinger == Fingers.Finger4)
+
+        public Fingers GetCurrentFingure()
         {
-            if (v == 1) return Input.GetKey(KeyCode.R);
-            if (v == 2) return Input.GetKey(KeyCode.O);
+            return RoundRequiredFingers[currentRound][currentFingureIndex];
         }
-        if (currentFinger == Fingers.Finger5)
+
+        private bool SetKeyImage(Fingers currentFinger, int v)
         {
-            if (v == 1) return Input.GetKey(KeyCode.T);
-            if (v == 2) return Input.GetKey(KeyCode.P);
+            if (currentFinger == Fingers.Finger1)
+            {
+                if (v == 1) Q = true;
+
+                
+                if (v == 1) T = false;
+            }
+            if (currentFinger == Fingers.Finger2)
+            {
+                if (v == 1) W = true;
+
+                if (v == 1) Q = false;
+                if (v == 1) E = false;
+                if (v == 1) R = false;
+                if (v == 1) T = false;
+            }
+            if (currentFinger == Fingers.Finger3)
+            {
+                if (v == 1) E = true;
+
+                if (v == 1) Q = false;
+                if (v == 1) W = false;
+                if (v == 1) R = false;
+                if (v == 1) T = false;
+            }
+            if (currentFinger == Fingers.Finger4)
+            {
+                if (v == 1) R = true;
+
+                if (v == 1) Q = false;
+                if (v == 1) W = false;
+                if (v == 1) E = false;
+                if (v == 1) T = false;
+            }
+            if (currentFinger == Fingers.Finger5)
+            {
+                if (v == 1) T = true;
+
+                if (v == 1) Q = false;
+                if (v == 1) W = false;
+                if (v == 1) E = false;
+                if (v == 1) R = false;
+            }
+            return false;
         }
-        return false;
+
+        private bool GetKeyWasPressed(Fingers currentFinger, int v)
+        {
+            if (currentFinger == Fingers.Finger1)
+            {
+                if (v == 1) return Input.GetKey(KeyCode.Q);
+                if (v == 2) return Input.GetKey(KeyCode.Y);
+            }
+            if (currentFinger == Fingers.Finger2)
+            {
+                if (v == 1) return Input.GetKey(KeyCode.W);
+                if (v == 2) return Input.GetKey(KeyCode.U);
+            }
+            if (currentFinger == Fingers.Finger3)
+            {
+                if (v == 1) return Input.GetKey(KeyCode.E);
+                if (v == 2) return Input.GetKey(KeyCode.I);
+            }
+            if (currentFinger == Fingers.Finger4)
+            {
+                if (v == 1) return Input.GetKey(KeyCode.R);
+                if (v == 2) return Input.GetKey(KeyCode.O);
+            }
+            if (currentFinger == Fingers.Finger5)
+            {
+                if (v == 1) return Input.GetKey(KeyCode.T);
+                if (v == 2) return Input.GetKey(KeyCode.P);
+            }
+            return false;
+        }
     }
 }
 
---
+++ Five Finger Fillet/Assets/Scripts/ToggleKeyPrompt.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SETS;
+
+public class ToggleKeyPrompt : MonoBehaviour
+{
+
+    public Sets sets;
+    private Image image;
+    public GameObject Q;
+    public GameObject W;
+    public GameObject E;
+    public GameObject R;
+    public GameObject T;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (sets.Q == true)
+            Q.SetActive(true);
+        else if (!sets.Q)
+        {
+            Q.SetActive(false);
+        }
+
+        if (sets.W == true)
+            W.SetActive(true);
+        else if (!sets.W)
+        {
+            W.SetActive(false);
+        }
+
+        if (sets.E == true)
+            E.SetActive(true);
+        else if (!sets.E)
+        {
+            E.SetActive(false);
+        }
+
+        if (sets.R == true)
+            R.SetActive(true);
+        else if (!sets.R)
+        {
+            R.SetActive(false);
+        }
+
+        if (sets.T == true)
+            T.SetActive(true);
+        else if (!sets.T)
+        {
+            T.SetActive(false);
+        }
+
+    }
+}
